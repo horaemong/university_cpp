97a35681b6db2b847c3b63403380081fe5e0fee0
laptop <jaeho3836@gmail.com>
2025-09-12 21:47:44 +0900

    11

--- 4주차/TVUser.cpp
+++ 4주차/TVUser.cpp
@@ -1,45 +1,43 @@
-//#include "tv.h"
-//
-//
-//int main(void) {
-//	int num = 0;
-//	bool a=true;
-//	Tv tv;
-//	while (a)
-//	{
-//		num = 0;
-//		cout << "\n1. tv의 현재 상태\n2. tv 전원버튼 누르기\n3.채널 올리기\n4. 채널 내리기\n5.볼륨 올리기\n6.볼륨 내리기\n7.프로그램 종료\n원하는 번호를 선택해주세요.\n";
-//		cin >> num;
-//
-//		switch (num)
-//		{
-//		default:
-//			return 0;
-//			break;
-//		case 1:
-//			tv.showStatus();
-//			break;
-//		case 2:
-//			tv.pushPower();
-//			break;
-//		case 3:
-//			tv.channelUp();
-//			break;
-//		case 4:
-//			tv.channelDown();
-//			break;
-//		case 5:
-//			tv.volumeUp();
-//			break;
-//		case 6:
-//			tv.volumeDown();
-//			break;
-//		case 7:
-//			a = false;
-//			break;
-//		}
-//
-//	}
-// return 0;
-//}
-//
+#include "tv.h"
+
+int main(void)
+{
+  int num = 0;
+  bool a = true;
+  Tv tv;
+  while (a)
+  {
+    num = 0;
+    cout << "\n1. tv의 현재 상태\n2. tv 전원버튼 누르기\n3.채널 올리기\n4. 채널 내리기\n5.볼륨 올리기\n6.볼륨 내리기\n7.프로그램 종료\n원하는 번호를 선택해주세요.\n";
+    cin >> num;
+
+    switch (num)
+    {
+    default:
+      return 0;
+      break;
+    case 1:
+      tv.showStatus();
+      break;
+    case 2:
+      tv.pushPower();
+      break;
+    case 3:
+      tv.channelUp();
+      break;
+    case 4:
+      tv.channelDown();
+      break;
+    case 5:
+      tv.volumeUp();
+      break;
+    case 6:
+      tv.volumeDown();
+      break;
+    case 7:
+      a = false;
+      break;
+    }
+  }
+  return 0;
+}
--- 4주차/circle.cpp
+++ 4주차/circle.cpp
@@ -1,34 +1,32 @@
-//#include <iostream>
-//using std::cout;
-//using std::endl;
-//
-//class Circle
-//{
-//public:
+#include <iostream>
+using std::cout;
+using std::endl;
 
-//	void setRadius(double a)
-//	{
-//		radius = a;
-//		cout << "반지름을 설정했습니다." << endl;
-//	}
-//	double getArea(void) {
-//		return radius * radius * pi;
-//	}
-//
-// int radius;
-//	const double pi = 3.14;
-//};
-//
-//int main(void)
-//{
-//	Circle c1;
-//	Circle c10;
-//
-//	c1.setRadius(1);
-//	c10.setRadius(10);
-//
-//	cout <<"원c1의 넓이 : " << c1.getArea() << endl;
-//	cout << "원c10의 넓이 : " << c10.getArea() << endl;
-//}
-//
-//
+class Circle
+{
+public:
+	void setRadius(double a)
+	{
+		radius = a;
+		cout << "반지름을 설정했습니다." << endl;
+	}
+	double getArea(void)
+	{
+		return radius * radius * pi;
+	}
+
+	int radius;
+	const double pi = 3.14;
+};
+
+int main(void)
+{
+	Circle c1;
+	Circle c10;
+
+	c1.setRadius(1);
+	c10.setRadius(10);
+
+	cout << "원c1의 넓이 : " << c1.getArea() << endl;
+	cout << "원c10의 넓이 : " << c10.getArea() << endl;
+}
